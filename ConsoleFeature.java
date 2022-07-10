1edcba7b474c6176f60648c960b3a861ea942037
Matt Decker <mdecker@gmail.com>
2022-07-09 20:52:42 -0500

    Updated Features

---
+++ ConsoleFeature.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class ConsoleFeature {
+    
+    //Method Creates User Input Option for Feet
+    public byte UserInputFeet() {
+
+        //Create Scanner Object
+        Scanner myObj = new Scanner(System.in);
+        //Prompt User to Enter Height in Feet
+        System.out.print("Enter Feet: ");
+
+        //Read User Input
+        byte feet = myObj.nextByte();
+
+        return feet;
+    }
+
+    //Method Creates User Input Option for Inches
+    public byte UserInputInches() {
+
+        //Create Scanner Object
+        Scanner myObj = new Scanner(System.in);
+        //Prompt User to Enter Height in Inches
+        System.out.print("Enter Inches: ");
+
+        //Read User Input
+        byte inches = myObj.nextByte();
+
+        return inches;
+    }
+}
